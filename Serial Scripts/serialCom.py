4d8327f079417ba628ec18fba48ac91424227def
amandahua17 <39344015+amandahua17@users.noreply.github.com>
2020-02-11 16:02:54 -0600

    script mostly working now

--- Serial Scripts/serialCom.py
+++ Serial Scripts/serialCom.py
@@ -1,11 +1,11 @@
 import serial
 print("Enter the Device number for the FPGA please, this will be an int")
 devNum = input()
-dev = '/dev/ttyUSB' + devNum
+dev = 'COM' + devNum
 print(dev)
 ser = serial.Serial(dev)  # open serial port, USB is the device found, may need to change the 0 to some other number
 ser.baudrate = 115200
-print(ser.name)         # check which port was really used
+print(ser)         # check which port was really used
 count = 0
 bytestring = b'1'
 while count < 4095:
@@ -13,12 +13,16 @@ while count < 4095:
     count += 1
 print (bytestring)
 numOfBytes = ser.write(bytestring)     # write a string
+print("numOfBytes, bytestringCount: ")
+print(numOfBytes)
+print(len(bytestring))
 
-if(numOfBytes == bytestring.count):
+if(numOfBytes == len(bytestring)):
     print("Success")
 else:
     print("Failed")
-bytesRead = ser.read(bytestring.count)
+
+bytesRead = ser.read(numOfBytes)
 print("Bytes Read:")
 print(bytesRead)
 ser.close()             # close port
