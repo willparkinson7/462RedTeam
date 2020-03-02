f71ea81226397eaa69f6a4c03f3c0650dc6e4952
amandahua17 <39344015+amandahua17@users.noreply.github.com>
2020-03-02 10:41:05 -0600

    updated eprom for monitor program, buggy base works

--- eprom.asm
+++ eprom.asm
@@ -1,14 +1,48 @@
-	la r26, BOT ;
+	la r25, QMC ;
+	la r22, BOT ;
 	la r31, TOP ;
-	la r30, -32 ;
-	la r29, -28 ;
-	la r28, -24 ;
-	la r27, -20 ;
+	la r30, -32 ;	TX_BUSY_FLAG
+	la r29, -28 ;	TX_DATA
+	la r28, -24 ;	RX_DATA_FLAG
+	la r27, -20 ;	RX_DATA
 
-TOP:	ld r1, 0(r28);
-	brzr r31, r1 ;
-	ld r3, 0(r27) ;
-BOT:	ld r2, 0(r30) ;
-	brnz r26, r2 ;
-	st r3, 0(r29) ;
-	br r31 ;
+TOP:	ld r1, 0(r28);	put rx_data_flag into r1
+	brzr r31, r1 ;  branch to top if r1 is zero
+	ld r1, 0(r27) ;		put rx_data into r1
+	addi r2, r1, -63 ; 	put ? ascii check into r2
+	brzr r25, r2 ;		go to ? assembly if difference is zero
+BOT:	ld r2, 0(r30) ;		
+	brnz r22, r2 ;
+	st r1, 0(r29) ;		spit letter back out
+	br r31 ;		go to top and get new rx_data
+QMC:    ld r1, 0(r30) ;
+	brnz r25, r1 ;		wait until tx_busy_flag is low
+	addi r2, r0, 82 ;	R	
+	st r2, (r29) ;		write to tx_data
+	addi r2, r0, 73 ;	I
+	st r2, (r29) ;		write to tx_data
+	addi r2, r0, 67 ;	C
+	st r2, (r29) ;		write to tx_data
+	addi r2, r0, 72 ;	H
+	st r2, (r29) ;		write to tx_data
+	addi r2, r0, 65 ;	A
+	st r2, (r29) ;		write to tx_data
+	addi r2, r0, 82 ;	R
+	st r2, (r29) ;		write to tx_data
+	addi r2, r0, 68 ;	D
+	st r2, (r29) ;		write to tx_data
+	addi r2, r0, 85 ;	U
+	st r2, (r29) ;		write to tx_data
+	addi r2, r0, 73 ;	I
+	st r2, (r29) ;		write to tx_data
+	addi r2, r0, 78 ;	N
+	st r2, (r29) ;		write to tx_data
+	addi r2, r0, 79 ;	O
+	st r2, (r29) ;		write to tx_data
+	addi r2, r0, 32 ;	_
+	st r2, (r29) ;		write to tx_data
+	addi r2, r0, 86 ;	V
+	st r2, (r29) ;		write to tx_data
+	addi r2, r0, 50 ;	2
+	st r2, (r29) ;		write to tx_data
+	br r31 ;	
--- eprom.vhd
+++ eprom.vhd
@@ -23,18 +23,53 @@ BEGIN
 
    WITH sel  SELECT
    data <=
-      X"2fc00014" WHEN X"00000000" , 
-      X"2f400010" WHEN X"00000004" , 
-      X"28400000" WHEN X"00000008" , 
-      X"309ffff0" WHEN X"0000000c" , 
-      X"379fffec" WHEN X"00000010" , 
-      X"187c0000" WHEN X"00000014" , 
-      X"6fbc0004" WHEN X"00000018" , 
-      X"a0fc2000" WHEN X"0000001c" , 
-      X"403e3003" WHEN X"00000020" , 
-      X"68420001" WHEN X"00000024" , 
-      X"403a0001" WHEN X"00000028" , 
-      X"f8000000" WHEN X"0000002c" , 
+      X"2e400040" WHEN X"00000000" , 
+      X"2d800030" WHEN X"00000004" , 
+      X"2fc0001c" WHEN X"00000008" , 
+      X"2f81ffe0" WHEN X"0000000c" , 
+      X"2f41ffe4" WHEN X"00000010" , 
+      X"2f01ffe8" WHEN X"00000014" , 
+      X"2ec1ffec" WHEN X"00000018" , 
+      X"08780000" WHEN X"0000001c" , 
+      X"403e1002" WHEN X"00000020" , 
+      X"08760000" WHEN X"00000024" , 
+      X"6883ffc1" WHEN X"00000028" , 
+      X"40322002" WHEN X"0000002c" , 
+      X"08bc0000" WHEN X"00000030" , 
+      X"402c2003" WHEN X"00000034" , 
+      X"187a0000" WHEN X"00000038" , 
+      X"403e0001" WHEN X"0000003c" , 
+      X"087c0000" WHEN X"00000040" , 
+      X"40321003" WHEN X"00000044" , 
+      X"68800052" WHEN X"00000048" , 
+      X"18ba0000" WHEN X"0000004c" , 
+      X"68800049" WHEN X"00000050" , 
+      X"18ba0000" WHEN X"00000054" , 
+      X"68800043" WHEN X"00000058" , 
+      X"18ba0000" WHEN X"0000005c" , 
+      X"68800048" WHEN X"00000060" , 
+      X"18ba0000" WHEN X"00000064" , 
+      X"68800041" WHEN X"00000068" , 
+      X"18ba0000" WHEN X"0000006c" , 
+      X"68800052" WHEN X"00000070" , 
+      X"18ba0000" WHEN X"00000074" , 
+      X"68800044" WHEN X"00000078" , 
+      X"18ba0000" WHEN X"0000007c" , 
+      X"68800055" WHEN X"00000080" , 
+      X"18ba0000" WHEN X"00000084" , 
+      X"68800049" WHEN X"00000088" , 
+      X"18ba0000" WHEN X"0000008c" , 
+      X"6880004e" WHEN X"00000090" , 
+      X"18ba0000" WHEN X"00000094" , 
+      X"6880004f" WHEN X"00000098" , 
+      X"18ba0000" WHEN X"0000009c" , 
+      X"68800020" WHEN X"000000a0" , 
+      X"18ba0000" WHEN X"000000a4" , 
+      X"68800056" WHEN X"000000a8" , 
+      X"18ba0000" WHEN X"000000ac" , 
+      X"68800032" WHEN X"000000b0" , 
+      X"18ba0000" WHEN X"000000b4" , 
+      X"403e0001" WHEN X"000000b8" , 
       X"00000000" WHEN OTHERS ;
 
    readprocess:PROCESS(ce_l,oe_l,data)
@@ -42,7 +77,7 @@ BEGIN
       IF (ce_l = '0' AND oe_l = '0') THEN
          d(31 DOWNTO 0) <= data ;
       else
-	       d(31 DOWNTO 0) <= (OTHERS => 'Z') ;
+	 d(31 DOWNTO 0) <= (OTHERS => 'Z') ;
       END IF;
    END PROCESS readprocess ;
 
